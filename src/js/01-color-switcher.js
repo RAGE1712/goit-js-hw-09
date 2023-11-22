4a0acfd7c0a595c0e74d5a96b8e0bb9277eeed43
RAGE1712 <137414196+RAGE1712@users.noreply.github.com>
2023-11-22 18:40:50 +0200

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+// Напиши скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону < body > 
+// на випадкове значення, використовуючи інлайн стиль. 
+// Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const refs = {
+    body: document.querySelector('body'),
+    startButton: document.querySelector('button[data-start]'),
+    stopButton : document.querySelector('button[data-stop]')
+}
+
+refs.startButton.addEventListener('click', () => {
+    timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.startButton.disabled = true;
+})
+
+refs.stopButton.addEventListener('click', () => {
+    clearInterval(timerId);
+    refs.startButton.disabled = false;
+});
+
+
 
---
+++ src/js/02-timer.js
@@ -0,0 +1,77 @@
+// Напиши скрипт таймера, який здійснює зворотний відлік до певної дати.
+// Такий таймер може використовуватися у блогах та інтернет - магазинах, сторінках реєстрації подій,
+// під час технічного обслуговування тощо.Подивися демо - відео роботи таймера.
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-aio-3.2.6.min.js';
+
+const refs = {
+    startButton: document.querySelector('button'),
+    day: document.querySelector('span[data-days]'),
+    hours: document.querySelector('span[data-hours]'),
+    minutes: document.querySelector('span[data-minutes]'),
+    seconds: document.querySelector('span[data-seconds]')
+}
+refs.startButton.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0] > new Date()) {
+            refs.startButton.disabled = false;
+            refs.startButton.addEventListener('click', startTimer);
+            function startTimer() {
+                Notiflix.Notify.info('Timer Started');
+                refs.startButton.disabled = true;
+                const timerId = setInterval(() => {
+                    const timer = convertMs(
+                      selectedDates[0] - new Date()
+                    );
+                refs.day.textContent = `${timer.days}`;
+                refs.hours.textContent = `${timer.hours}`;
+                refs.minutes.textContent = `${timer.minutes}`;
+                    refs.seconds.textContent = `${timer.seconds}`;
+                    if (selectedDates[0] - new Date() < 1000) {
+                        clearInterval(timerId);
+                        refs.startButton.disabled = false;
+                        Notiflix.Notify.success('Timer Ends');
+                    }
+                }, 1000);
+                
+            }
+        } else {
+            refs.startButton.disabled = true;
+            Notiflix.Notify.failure('Please choose a date in the future');
+        }
+  },
+};
+
+flatpickr('input[type="text"]', options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
